f74aa6562cd9aa50136cd94a53e45370043155b3
Александр <nikol.alex06@mail.ru>
2024-10-04 20:58:48 +0300

    Начал делать страницу и списки сертификатов и наград как компонент

--- src/app/about/fiddback/page.js
+++ src/app/about/fiddback/page.js
@@ -1,6 +1,21 @@
+'use client'
+
+import ListCertificate from "@/app/components/listCertificate";
+import { useState } from "react";
+
 export default function Fiddback () {
+    const [list, setList] = useState([1, 2, 3])
+
     return (
-    <>
-    </>
+    <div className="flex flex-col gap-y-6">
+        <h1 className="text-dark-blue font-bold text-title">Отзывы наших клиентов</h1>
+        <p className="text-base font-bold">Компания <span className="text-blue font-bold text-minititle">Неотехника</span> глубоко ценит отношения со своими партнерами и всегда готова к <span className="text-blue font-bold text-minititle">расширению сотрудничества.</span></p>
+        <div className="grid grid-rows-2 grid-cols-5 w-full h-[390px] gap-3">
+            <div className="w-full row-span-2 col-span-3 overflow-hidden"><div className="bg-cover bg-no-repeat bg-center hover:scale-105 w-full h-full duration-300 cursor-pointer" style={{backgroundImage: `url(/fiddback${list[0]}.jpg)`}}></div></div>
+            <div className="w-full row-span-1 col-span-2 overflow-hidden"><button className="bg-cover bg-no-repeat bg-center hover:scale-105 w-full h-full duration-300" style={{backgroundImage: `url(/fiddback${list[1]}.jpg)`}} onClick={() => setList([list[1], list[2], list[0]])}></button></div>
+            <div className="w-full row-span-1 col-span-2 overflow-hidden"><button className="bg-cover bg-no-repeat bg-center hover:scale-105 w-full h-full duration-300" style={{backgroundImage: `url(/fiddback${list[2]}.jpg)`}} onClick={() => setList([list[2], list[0], list[1]])}></button></div>
+        </div>
+        <ListCertificate name={'fiddbackCert'} start={1} end={13}/>
+    </div>
     )
 }
--- src/app/components/photoLink.js
+++ src/app/components/photoLink.js
@@ -3,11 +3,8 @@
 export default function PhotoLinks() {
     return (
         <div className="grid grid-cols-6 grid-rows-5  h-[638px] w-full gap-5">
-            <button className="col-span-3 row-span-3 overflow-hidden relative" onClick={() => {
-                document.getElementById('1');
-                console.log('here')
-            }}>
-                <div className="absolute w-full h-full top-0 bg-opacity-0 active:bg-opacity-100"><div id='1' onClick={() => console.log('and here')} className="translate-x-full text-blue text-minititle font-bold hover:translate-x-0 active:translate-x-0 focus:translate-x-0">Комплектные электростанции в цельнометаллических блок-контейнерах</div></div>
+            <button className="col-span-3 row-span-3 overflow-hidden relative">
+                {/* <div className="absolute w-full h-full top-0 bg-opacity-0 active:bg-opacity-100"><div id='1' onClick={() => console.log('and here')} className="translate-x-full text-blue text-minititle font-bold hover:translate-x-0 active:translate-x-0 focus:translate-x-0">Комплектные электростанции в цельнометаллических блок-контейнерах</div></div> */}
                 <div className="bg-cover bg-no-repeat bg-center w-full h-full hover:scale-105 duration-300" style={{backgroundImage: 'url(mainPhoto1.jpg)'}}></div>
             </button>
             <div className="col-span-3 row-span-3 overflow-hidden">
