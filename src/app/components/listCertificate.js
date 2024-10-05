4490c5be57e4c04c8220e35a011ac687d406b30a
Александр <nikol.alex06@mail.ru>
2024-10-05 10:54:56 +0300

    Доделал компонент списка сертификатов

--- src/app/components/listCertificate.js
+++ src/app/components/listCertificate.js
@@ -1,4 +1,10 @@
+'use client'
+
+import { BaseButton } from "./buttons";
+import { use, useState } from "react";
+
 export default function ListCertificate ({name, start, end}) {
+    const [isOpen, setIsOpen] = useState(false)
     let list = [];
 
     for(let i = start; i <= end; i++){
@@ -6,12 +12,17 @@ export default function ListCertificate ({name, start, end}) {
     }
 
     return (
-        <div className="grid grid-cols-3 w-full gap-3 h-[530px] overflow-y-scroll">
-            {list.map(n => {
-                return (
-                    <button key={n} className="h-[464px] w-full bg-cover bg-no-repeat bg-center bord_frame" style={{backgroundImage: `url(/${name}${n}.jpg)`}}></button>
-                )
-            })}
+        <div className={`${isOpen ? 'h-auto flex flex-col gap-y-4' : 'h-[530px]'} overflow-y-hidden w-full relative transition-all duration-200`}>
+            <div className="grid grid-cols-3 w-full gap-3">
+                {list.map(n => {
+                    return (
+                        <button key={n} className="h-[464px] w-full bg-cover bg-no-repeat bg-center bord_frame" style={{backgroundImage: `url(/${name}${n}.jpg)`}}></button>
+                    )
+                })}
+            </div>
+            <div className={`${isOpen ? '' : "absolute bottom-0 w-full button-blue-bg pt-32"} flex justify-center`}>
+                <BaseButton text={isOpen ? 'Скрыть' : 'Показать'} isLink={false} action={() => setIsOpen(!isOpen)}/>
+            </div>
         </div>
     )
 }
